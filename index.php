cd030913565f34efca279931bcf5779d1ec5d9a7
Rasyiqal F <98671712+rasyiqal@users.noreply.github.com>
2023-02-05 19:52:39 +0700

    Update index.php

--- index.php
+++ index.php
@@ -5,6 +5,13 @@ require_once("auth.php");
 <!DOCTYPE html>
 <html lang="en">
 
+<?php
+require_once("auth.php");
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -28,6 +35,7 @@ require_once("auth.php");
         }
     </style>
 </head>
+
 
 <body>
     <div id="app">
@@ -211,6 +219,7 @@ require_once("auth.php");
                                             </table>
                                         </div>
                                     </div>
+                                    <?php endforeach ?>
                                 </div>
                             </div>
                         </div>
